fab9682c49e3a712f0395b1f6d637f35dd2f2381
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-04-24 18:12:14 +0200

    Added player controller

---
+++ Assets/Scripts/Luca/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+
+    [SerializeField] Transform toFollow;
+    Vector3 displacement;
+
+	void Start () {
+        displacement = transform.position - toFollow.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = toFollow.position + displacement;
+	}
+}
---
+++ Assets/Scripts/Luca/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] float speed;
+    [SerializeField] float drag = 0.9f;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+        if(input.magnitude > 1)
+        {
+            input.Normalize();
+        }
+
+        rb.velocity *= drag;
+        rb.velocity = input * speed;
+
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 30), "Player Velocity: " + rb.velocity);
+    }
+}
