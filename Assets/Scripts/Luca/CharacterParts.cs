90a631747defe774b85ffee919af30e118d0dc62
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-04-25 12:29:35 +0200

    Reorganized scripts

---
+++ Assets/Scripts/Luca/CharacterParts.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterParts : MonoBehaviour {
+
+    public static CharacterParts Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("2 CharacterParts objects present");
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
